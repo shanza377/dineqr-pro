5029e921bd0d4ac2005d476ebfb2682926257861
Shanza Hussain <qashan866@gmail.com>
2026-04-21 12:56:48 +0500

    Fix: Use correct Vercel Analytics for React

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import { Analytics } from "@vercel/analytics/next"
-import React from 'react'
+import { Analytics } from "@vercel/analytics/react"
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
