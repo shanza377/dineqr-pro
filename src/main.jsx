aebccbac852f5a617e5468f137b3370c11d7f4c2
Shanza Hussain <qashan866@gmail.com>
2026-04-21 12:30:53 +0500

    Added Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -28,5 +29,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         },
       }}
     />
+    <Analytics />
   </React.StrictMode>,
 )
